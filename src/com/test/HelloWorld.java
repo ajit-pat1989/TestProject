0252a81fcc47255c11762abc661a7ed3182a41a2
DELL <ajitkumar.patil@outlook.com>
2020-06-11 21:16:17 +0530

    test project for jenkins

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import sun.applet.Main;
+
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World program.....");
+    }
+}
